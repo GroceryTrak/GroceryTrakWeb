ef9730eaf7778dd37d6829f1c3c0bad7ac06193b
Khoa <khoadd2003@gmail.com>
2025-02-07 05:00:15 -0500

    add camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:grocery_trak_web/home.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(const MyApp());
+// Global variable to hold available cameras.
+late List<CameraDescription> cameras;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Grocery Trak',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(
+        title: 'Grocery Trak',
+        camera: cameras.first, // Pass the first available camera.
       ),
-      home: MyHomePage(title: 'Home - Grocery Trak'),
-      debugShowCheckedModeBanner: false, // remove debug banner
     );
   }
 }
